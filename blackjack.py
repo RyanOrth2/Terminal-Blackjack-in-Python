62becb542c3bcd33abd7acfdc267f3fa0f89bf40
Ryan T Orth <133438242+R-Orth@users.noreply.github.com>
2024-08-09 14:50:18 -0600

    Update blackjack.py

--- blackjack.py
+++ blackjack.py
@@ -1,70 +1,92 @@
 import random
 import os
+import time
 from art import *
+from helper_functions import Hand, Card
 
 
 def clear():
     os.system('cls')
 
 
-def printHand(hand):
-    for x in hand:
-        print(".------.", end = " ")
-    print('')
-    for x in hand:
-        print("|" + numToCard(x) + "/\\  |", end = " ")
-    print('')
-    for x in hand:
-        print("| /  \\ |", end = " ")
-    print('')
-    for x in hand:
-        print("| \\  / |", end = " ")
-    print('')
-    for x in hand:
-        print("|  \\/" + numToCard(x) + "|", end = " ")
-    print('')
-    for x in hand:
-        print("'------'", end = " ")
-    print('')
+def printGame(player, dealer):
+    Hand.__print__(dealer)
+    print('\n\n\n\n\n')
+    Hand.__print__(player)
 
 
-def numToCard(x):
-    if x == 10:
-        return '10'
-    
-    retVal = ''
-
-    if x == 13:
-        retVal += 'K '
-    elif x == 12:
-        retVal += 'Q '
-    elif x == 11:
-        retVal += 'J '
-    elif x == 1:
-        retVal += 'A '
-    else:
-        retVal += str(x) + ' '
-
-    return retVal
-
-
-def hit(hand):
-    hand.append(random.randint(1, 13))
+def hit(person):
+    Hand.add(person)
 
 
 def deal(player, dealer):
     player.clear()
     dealer.clear()
 
-    player.append(random.randint(1,13))
-    dealer.append(random.randint(1,13))
-    player.append(random.randint(1,13))
+    Hand.add(player)
+    Hand.add(dealer)
+    Hand.add(player)
 
 
-player = []
-dealer = []
+    def playRound(self, player, dealer):
+        while player.sum() < 22 and (playerIn != 'st' or playerIn != 'q'):
+            printGame(player, dealer)
+            print("\nYou have ", end = " ")
+            print(player.sum(), end = " ")
+            print("against the Dealers' ", end = " ")
+            print(dealer.sum())
+
+            if player.sum() < 21:
+                playerIn = input("\nWhat would you like to do?\t(h)it / (st)and / (d)ouble / (sp)lit / (q)uit\n")
+
+                while playerIn != 'h' and playerIn != 'st' and playerIn != 'd' and playerIn != 'sp' and playerIn != 'q':
+                    playerIn = input("\nSorry, we didn\'t recognize that one...\n\nWhat would you like to do?\t(h)it / (st)and / (d)ouble / (sp)lit / (q)uit\n")
+
+                match (playerIn):
+                    case "h":
+                        player.add()
+                    case "st":
+                        continue
+                    case "d":
+                        player.add()
+                        playerIn = "st"
+
+
+player = Hand()
+dealer = Hand()
+playerIn = 'z'
+gameIn = 'z'
 
 print(logo)
+time.sleep(3)
 print('\n\n\n\n\n')
 
-print('Welcome to Terminal Blackjack!\n\nWould you like to play?')
+gameIn = input('Welcome to Terminal Blackjack!\n\nWould you like to play?    (y/n)\n\n')
+
+while gameIn != 'y' and gameIn != 'n':
+    gameIn = input('\nSorry, we didn\'t recognize that one...\n\nWould you like to play?    (y/n)\n\n')
+
+if gameIn == 'n':
+    exit(1)
+
+print("\n\nRight on! We'll deal you in!")
+time.sleep(3) 
+clear()
+deal(player, dealer)
+
+
+while gameIn != 'q':
+
+    
+    
+
+
+
+    userIn = input("Would you like to play again?   (y/n)\n\n")
+    
+    while userIn != 'y' and userIn != 'n':
+        userIn = input('\nSorry, we didn\'t recognize that one...\n\nWould you like to play again?    (y/n)\n\n')
+
+    if userIn == 'n':
+        print('\n\nSee you next time!')
+        exit(1)
